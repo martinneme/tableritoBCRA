4bd016720497aea4356be2d66b54111f581c9687
unknown <martinneme@outlook.com>
2024-08-29 16:53:54 -0300

    modif

--- app/page.tsx
+++ app/page.tsx
@@ -8,8 +8,9 @@ export default function Home() {
   
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex titleProyect">
         <div className="left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30" style={{ width: '90%',margin:'auto'}}>
-          <code className="font-mono font-bold text-2xl lg:text-4xl">
-            Tablerito BCRA
+          <code className="mb4 text-4xl font-bold uppercase text-white sm:text-5xl md:mb-8 md:text-6xl titleProyect">
+            <b className="bcra">Tablerito</b>
+            <b > BCRA</b>
           </code>
           <div className="creator">
       <p><i>Created by </i></p>
